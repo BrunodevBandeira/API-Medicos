fb25602b4ced6e73fc6676daff51c649c97ee1da
brunobandeira <brunodev29ab@gmail.com>
2023-03-12 10:42:37 -0300

    First commit

---
+++ src/main/java/med/voll/api/endereco/DadosCadastroPaciente.java
@@ -0,0 +1,10 @@
+package med.voll.api.endereco;
+
+public record DadosCadastroPaciente(
+        String nome,
+        String email,
+        String telefone,
+        String cpf,
+        DadosEndereco endereco
+) {
+}
